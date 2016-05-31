a819f030d3d77bc39f528e7de23a8b972aac9f8f
Jannik Weichert <j.weichert@hochzehn.com>
2016-05-31 11:27:07 +0200

    Added default values and messages, if no environment has not been set properly. Renamed restmq to restmq_ip for clarification. Added jobs environment to specificy number of parallel jobs externally.

--- bin/run/parse.sh
+++ bin/run/parse.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
+# Defaults
+JOBS_DEFAULT=50
+NUM_DEFAULT=10
+RESTMQ_IP_DEFAULT=127.0.0.1
 # Parameters
 
 NUM=$1
 if [ -z $NUM ]; then
-  NUM=10
+  echo "Number of domains not specified with argument \$1. Using default: $NUM_DEFAULT"
+  NUM=$NUM_DEFAULT
+fi
+
+if [ -z $jobs ]; then
+    echo "Number of parallel jobs for curl to restMQ not specified with environment \$jobs. Using default: $JOBS_DEFAULT"
+    jobs=$JOBS_DEFAULT
+fi
+
+if [ -z $restmq_ip ]; then
+    echo "IP of restMQ not specified with environment \$restmq_ip. Using default: $RESTMQ_IP_DEFAULT"
+    restmq_ip=$RESTMQ_IP_DEFAULT
 fi
 
 # Constants
@@ -16,7 +31,7 @@ DOMAIN_FILE="tmp/domains.txt"
 
 head -n "$NUM" "$INPUT_FILE" > "$INTERMEDIATE_FILE"
 
-rm -f $DOMAIN_FILE 2> dev/null
+rm -f $DOMAIN_FILE 2> /dev/null
 
 while read entry; do
   values=(${entry//,/ })
@@ -31,4 +46,4 @@ done < "$INTERMEDIATE_FILE"
 echo "DOMAIN FILE: "
 cat $DOMAIN_FILE
 
-cat $DOMAIN_FILE | parallel --jobs 100 curl -s -X POST -d "value={}" "http://$restmq:8888/q/domains"
+cat $DOMAIN_FILE | parallel --jobs $jobs curl -s -X POST -d "value={}" "http://$restmq_ip:8888/q/domains" 1> /dev/null
